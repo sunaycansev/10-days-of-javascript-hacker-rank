3f2eec7e480c7def7a98a715a28ceb34b83a454c
sunaycansev <fsunaycansev@gmail.com>
2021-07-20 15:38:40 +0300

    add day-4 solutions

---
+++ Day 4/count-objects.js
@@ -0,0 +1,15 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+  let count = 0;
+  for (let obj of objects) {
+    if (obj.x == obj.y) {
+      count += 1;
+    }
+  }
+  return count;
+}
